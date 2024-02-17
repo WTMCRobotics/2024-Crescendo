aa9d8526f97caea07cce377b34401e474a1e7139
team6101 <wtmcrobotics@wccnet.edu>
2024-02-17 15:25:10 -0500

    change autonroutes

--- src/main/java/frc/robot/auton/AutonRoutes.java
+++ src/main/java/frc/robot/auton/AutonRoutes.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class AutonRoutes {
 
     public static ArrayDeque<AutonAction> RUN_INTO_WALL_AND_BREAK_ROBOT = new ArrayDeque<>(
-        List.of(new AutonMoveForward(36), new AutonMoveForward(-36))
+        List.of(new AutonMoveForward(36))
     );
     public static ArrayDeque<AutonAction> RUN_INTO_OTHER_WALL_AND_BREAK_ROBOT_AGAIN = new ArrayDeque<>(
-        List.of(new AutonMoveForward(-36), new AutonMoveForward(36))
+        List.of(new AutonMoveForward(-36))
     );
 }
