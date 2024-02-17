0a82c49dafce0766aa9b7f39a0a24ab89e9eac45
team6101 <wtmcrobotics@wccnet.edu>
2024-02-17 13:09:34 -0500

    Add WIP autonShoot class

--- src/main/java/frc/robot/auton/AutonShoot.java
+++ src/main/java/frc/robot/auton/AutonShoot.java
@@ -1,13 +1,32 @@
 package frc.robot.auton;
 
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants;
+import frc.robot.motor.MotorController;
+
 //TODO : this
 public class AutonShoot extends AutonAction {
 
+    private MotorController leftShooterMotor;
+    private MotorController rightShooterMotor;
+
+    private MotorController shooterFeederMotor;
+
+    private double revFinishedTime;
+
     @Override
     public boolean isDone() {
+        if (Timer.getFPGATimestamp() >= revFinishedTime) {
+            shooterFeederMotor.set(0.2);
+        }
+
         return true;
     }
 
     @Override
-    public void initiate() {}
+    public void initiate() {
+        leftShooterMotor.set(1.0);
+        rightShooterMotor.set(1.0);
+        revFinishedTime = Timer.getFPGATimestamp() + Constants.REV_TIME;
+    }
 }
