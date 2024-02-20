37216761b39c86fee9e3cf812a3f78e6cc9c9517
team6101 <wtmcrobotics@wccnet.edu>
2024-02-20 16:41:32 -0500

    Fix autonshoot

--- src/main/java/frc/robot/InputtedCoDriverControls.java
+++ src/main/java/frc/robot/InputtedCoDriverControls.java
@@ -1,6 +1,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.auton.AutonShoot;
 
 public class InputtedCoDriverControls {
 
@@ -11,20 +12,25 @@ public class InputtedCoDriverControls {
     }
 
     public static void onEveryFrame() {
-        if (controller.getAButtonPressed()) {
-            dropIntoAmp();
+        if (controller.getAButton()) {
+            Robot.feederMotor.set(30);
+        } else {
+            Robot.feederMotor.set(0);
         }
         if (controller.getXButtonPressed()) {
             shootIntoSpeaker();
         }
-        if (controller.getPOV() == 0 || controller.getPOV() == 180) {
-            doIntake();
+        if (shoot != null) {
+            if (shoot.isDone() == true) {
+                shoot = null;
+            }
         }
+        doIntake();
         if (controller.getBackButton()) {
-            doClimb();
+            // doClimb();
         }
         if (controller.getStartButton()) {
-            extendArms();
+            // extendArms();
         }
     }
 
@@ -37,11 +43,20 @@ public class InputtedCoDriverControls {
     }
 
     private static void doIntake() {
-        throw new UnsupportedOperationException("Unimplemented method 'doIntake'");
+        if (controller.getPOV() == 0) {
+            Robot.rightShooterMotor.set(0.3);
+            Robot.leftShooterMotor.set(-0.3);
+        } else if (controller.getPOV() == 180) {
+            Robot.rightShooterMotor.set(0);
+            Robot.leftShooterMotor.set(0);
+        }
     }
 
+    static AutonShoot shoot = null;
+
     private static void shootIntoSpeaker() {
-        throw new UnsupportedOperationException("Unimplemented method 'shootIntoSpeaker'");
+        shoot = new AutonShoot();
+        shoot.initiate();
     }
 
     private static void dropIntoAmp() {
--- src/main/java/frc/robot/auton/AutonRoutes.java
+++ src/main/java/frc/robot/auton/AutonRoutes.java
@@ -1,5 +1,6 @@
 package frc.robot.auton;
 
+import frc.robot.Vision.AutonRotateAction;
 import java.util.ArrayDeque;
 import java.util.List;
 
@@ -10,5 +11,26 @@ public class AutonRoutes {
     );
     public static ArrayDeque<AutonAction> RUN_INTO_OTHER_WALL_AND_BREAK_ROBOT_AGAIN = new ArrayDeque<>(
         List.of(new AutonMoveForward(-36))
+    );
+
+    public static ArrayDeque<AutonAction> SHOOT_AND_RUN_INTO_OTHER_WALL_AND_BREAK_ROBOT = new ArrayDeque<AutonAction>(
+        List.of(new AutonShoot(), new AutonMoveForward(-36), new AutonRotate(90))
+    );
+
+    public static ArrayDeque<AutonAction> SHOOT_AND_RUN_INTO_OTHER_WALL_AND_BREAK_ROBOT = new ArrayDeque<AutonAction>(
+        List.of(new AutonShoot(), new AutonParallelAction(new AutonIntake(), new AutonMoveForward(-36)))
+    );
+
+    public static ArrayDeque<AutonAction> SHOOT_AND_RUN_INTO_OTHER_WALL_AND_BREAK_ROBOT = new ArrayDeque<AutonAction>(
+        List.of(new AutonShoot(), new AutonMoveForward(-36), new AutonRotate(90))
+    );
+
+    public static ArrayDeque<AutonAction> SHOOT_AND_RUN_INTO_OTHER_WALL_AND_BREAK_ROBOT = new ArrayDeque<AutonAction>(
+        List.of(
+            new AutonParallelAction(
+                new AutonSequentialAction(new AutonWait(1.5), new AutonIntake()),
+                new AutonMoveForward(-36)
+            )
+        )
     );
 }
