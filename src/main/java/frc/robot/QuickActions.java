401c4d1a665338fc2d927d5c7d7d7cc632918e67
team6101 <wtmcrobotics@wccnet.edu>
2024-02-28 19:55:37 -0500

    Rename stopAll to stopAllDriveMotors

--- src/main/java/frc/robot/QuickActions.java
+++ src/main/java/frc/robot/QuickActions.java
@@ -2,12 +2,12 @@ package frc.robot;
 
 public class QuickActions {
 
-    public static void setAll(double percent) {
+    public static void setDriveMotors(double percent) {
         Robot.motors.getDriveLeftParent().set(percent);
         Robot.motors.getDriveRightParent().set(percent);
     }
 
-    public static void stopAll() {
+    public static void stopDriveMotors() {
         Robot.motors.getDriveLeftParent().set(0.0);
         Robot.motors.getDriveRightParent().set(0.0);
     }
