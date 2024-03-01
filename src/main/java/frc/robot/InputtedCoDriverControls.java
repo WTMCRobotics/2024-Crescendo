1f863d5cccf8551075799a06724f2bba9adb64f2
team6101 <wtmcrobotics@wccnet.edu>
2024-02-29 19:44:39 -0500

    implement OOP based codriver controls

--- src/main/java/frc/robot/InputtedCoDriverControls.java
+++ src/main/java/frc/robot/InputtedCoDriverControls.java
@@ -1,9 +1,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.auton.AutonShoot;
-import frc.robot.auton.ExtendArms;
-import frc.robot.auton.RetractArms;
+import frc.robot.RobotComponents.Climber;
+import frc.robot.RobotComponents.Intake;
+import frc.robot.RobotComponents.Shooter;
 
 public class InputtedCoDriverControls {
 
@@ -14,51 +14,27 @@ public class InputtedCoDriverControls {
     }
 
     public static void onEveryFrame() {
-        Robot.motors.getFeeder().stopMotor();
-        if (controller.getAButton()) {
-            Robot.motors.getFeeder().set(30);
+        if (controller.getAButtonPressed()) {
+            Shooter.startFeedMotors();
         }
-        if (controller.getXButtonPressed()) {
-            shootIntoSpeaker();
+        if (controller.getXButton()) {
+            Shooter.startShooterMotors();
+        } else {
+            Shooter.stopShooterMotors();
         }
-        Robot.motors.getIntake().stopMotor();
+
         if (controller.getYButton()) {
-            doFloorIntake();
+            Intake.startFloorIntake();
+        } else {
+            Intake.stopFloorIntake();
         }
-        Robot.motors.getLeftClimb().stopMotor();
-        Robot.motors.getRightClimb().stopMotor();
+
         if (controller.getStartButton()) {
-            //extendArms();
-            Robot.motors.getLeftClimb().set(Constants.CLIMB_EXTENSION_SPEED);
-            Robot.motors.getRightClimb().set(Constants.CLIMB_EXTENSION_SPEED);
+            Climber.manualExtendArms();
+        } else if (controller.getBackButton()) {
+            Climber.manualRetractArms();
+        } else {
+            Climber.stopClimbMotors();
         }
-        if (controller.getBackButton()) {
-            //retractArms();
-            Robot.motors.getLeftClimb().set(Constants.CLIMB_RETRACTION_SPEED);
-            Robot.motors.getRightClimb().set(Constants.CLIMB_RETRACTION_SPEED);
-        }
-    }
-
-    private static void doFloorIntake() {
-        Robot.motors.getIntake().set(.5);
-        //Robot.motors.getFeeder().set(.5);
-    }
-
-    private static void extendArms() {
-        Robot.getTeleopActionRunner().addActionToRun(new ExtendArms());
-        Robot.getTeleopActionRunner().removeActionsOfType(RetractArms.class);
-    }
-
-    private static void retractArms() {
-        Robot.getTeleopActionRunner().addActionToRun(new RetractArms());
-        Robot.getTeleopActionRunner().removeActionsOfType(ExtendArms.class);
-    }
-
-    private static void shootIntoSpeaker() {
-        Robot.getTeleopActionRunner().addActionToRun(new AutonShoot());
-    }
-
-    private static void dropIntoAmp() {
-        throw new UnsupportedOperationException("Unimplemented method 'dropIntoAmp'");
     }
 }
--- src/main/java/frc/robot/RobotComponents/Shooter.java
+++ src/main/java/frc/robot/RobotComponents/Shooter.java
@@ -2,22 +2,27 @@ package frc.robot.RobotComponents;
 
 import frc.robot.Constants;
 import frc.robot.Robot;
+import frc.robot.auton.AutonShoot;
 import frc.robot.auton.AutonShooterFeed;
 
 public class Shooter {
 
-    public void startShooterMotors() {
+    public static void startShooterMotors() {
         Robot.motors.getLeftFlywheel().setVelocity(Constants.SHOOTING_VELOCITY);
         Robot.motors.getRightFlywheel().setVelocity(Constants.SHOOTING_VELOCITY);
     }
 
-    public void stopShooterMotors() {
+    public static void stopShooterMotors() {
         Robot.motors.getLeftFlywheel().stopMotor();
         Robot.motors.getRightFlywheel().stopMotor();
     }
 
-    public void startFeedMotors() {
+    public static void startFeedMotors() {
         Robot.getTeleopActionRunner().removeActionsOfType(AutonShooterFeed.class);
         Robot.getTeleopActionRunner().addActionToRun(new AutonShooterFeed());
+    }
+
+    public static void autoShootIntoSpeaker() {
+        Robot.getTeleopActionRunner().addActionToRun(new AutonShoot());
     }
 }
