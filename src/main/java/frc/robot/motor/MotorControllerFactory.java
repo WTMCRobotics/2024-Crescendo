aec3205bb5f063ea1dc3ec5750a61f6d543b3e85
team6101 <wtmcrobotics@wccnet.edu>
2024-03-05 15:28:23 -0500

    Add support for brushed & brushless motors

--- src/main/java/frc/robot/motor/MotorControllerFactory.java
+++ src/main/java/frc/robot/motor/MotorControllerFactory.java
@@ -1,13 +1,17 @@
 package frc.robot.motor;
 
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 public class MotorControllerFactory {
 
     public static MotorController create(int id, MotorController.Type type) {
         switch (type) {
             case Talon:
                 return new TalonMotorController(id);
-            case SparkMax:
-                return new SparkMotorController(id);
+            case SparkMaxBrushless:
+                return new SparkMotorController(id, MotorType.kBrushless);
+            case SparkMaxBrushed:
+                return new SparkMotorController(id, MotorType.kBrushed);
             default:
                 return null;
         }
